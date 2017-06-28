0285f1c38f6e65a1ed5f8346de1d881133eebe89
itzel <itzeljimenez.do@gmail.com>
2017-06-28 06:49:45 -0500

    Lista dinámica

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,21 @@
 var api={
 	url: 'https://laboratoria.cuadra.co/api/v1/students/'
-}
+};
 
+var plantilla= '<li><input type="checkbox"> **nombre** </li>'
 var cargarPagina = function(){
+	$.getJSON(api.url, function(response){
+		response.forEach(obtenerDatos);
+	});
+};
 
+var obtenerDatos = function(alumnas){
+	mostrarAlumnas(alumnas.name + " " + alumnas.pLastName + " " + alumnas.mLastName);
+};
+
+var mostrarAlumnas = function(nombreAlumnas){
+	var nuevaPlantilla = plantilla.replace("**nombre**", nombreAlumnas);
+	$('#lista-alumnas').append(nuevaPlantilla);
+	
 }
-
 $(document).ready(cargarPagina);
